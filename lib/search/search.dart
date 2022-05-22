d7a015be518379f0c00c99c84638ebe913dcfdb3
Ruslan Gaynullin <ruslan.gaynullin.23@gmail.com>
2022-05-22 16:56:59 +0300

    Notification and search added

---
+++ lib/search/search.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchDelegate extends SearchDelegate {
+  List<String> searchTerms = [
+    'Huseyin Onur Onma',
+    'Ruslan Gaynullin',
+    'Bahri Baran Coskun',
+    'Muslim Malsagov',
+    'Anas Alajati',
+  ];
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = '';
+          },
+          icon: const Icon(Icons.clear))
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var name in searchTerms) {
+      if (name.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(name);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var name in searchTerms) {
+      if (name.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(name);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+}
