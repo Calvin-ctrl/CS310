2962876547d021826cad27686be26eb7765af71c
Ruslan Gaynullin <ruslan.gaynullin.23@gmail.com>
2022-05-22 18:07:51 +0300

    custom nav bar is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cs310_feed_view/widget/custom_navbar.dart';
 import 'package:flutter/material.dart';
 import 'package:like_button/like_button.dart';
 
@@ -17,15 +18,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: HomeView(),
@@ -53,17 +45,9 @@ class HomeView extends StatelessWidget {
               },
               icon: const Icon(Icons.notification_add_outlined),
             ),
-            IconButton(
-              onPressed: () {
-                showSearch(
-                  context: context,
-                  delegate: CustomSearchDelegate(),
-                );
-              },
-              icon: const Icon(Icons.search),
-            ),
           ],
         ),
+        bottomNavigationBar: CustomNavBar(),
         body: _postListView());
   }
 
---
+++ lib/widget/custom_navbar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../search/search.dart';
+
+class CustomNavBar extends StatelessWidget {
+  const CustomNavBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Colors.black,
+      child: Container(
+        height: 70,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/');
+                }),
+            IconButton(
+              onPressed: () {
+                showSearch(
+                  context: context,
+                  delegate: CustomSearchDelegate(),
+                );
+              },
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+            ),
+            IconButton(
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/');
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
